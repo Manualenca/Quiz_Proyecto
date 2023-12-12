876f29bd4d9475debe741cc161e7cb80717e02ae
Mauri Gonzalez <maurigz1995@gmail.com>
2023-12-12 20:29:30 -0300

    Realize cambio en el proyecto completo y en Models

---
+++ QuizApp/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import (
+    CategoryPregunta,
+    SubcategoryPreguntas,
+    QuestionPreguntas,
+    Partida,
+    PreguntaPartida,
+    CustomUser,
+    Jugador,
+    Editor,
+)
+
+# Register your models here.
+
+admin.site.register(CategoryPregunta)
+admin.site.register(SubcategoryPreguntas)
+admin.site.register(QuestionPreguntas)
+admin.site.register(Partida)
+admin.site.register(PreguntaPartida)
+admin.site.register(CustomUser)
+admin.site.register(Jugador)
+admin.site.register(Editor)
