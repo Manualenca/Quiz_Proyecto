4a111d3b296070f8d5599083e2eb28a8952ec3d8
Maria Dolores <mdcostab@gmail.com>
2023-12-13 18:06:48 -0300

    Envío posibles vistas para el preguntado

--- Quiz/QuizApp/views.py
+++ Quiz/QuizApp/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from django.urls import path
+from . import views
+from django.views.generic import ListView, DetailView
+from .models import Question, Partida
 
-# Create your views here.
+def welcome(request):
+    now = datetime.datetime.now()
+    return HttpResponse(f"Hola, es hora de jugar al Preguntado. La hora del servidor es  {now.strftime('%A, %d %B, %Y a las %X')}.")
 
-def inicio(request):
 
-    return render(request,"QuizApp/base.html")
+class ListaPreguntasView(ListView):
+    model = Question
+    template_name = 'preguntas/lista_preguntas.html'
+    context_object_name = 'preguntas'
+
+class DetallePreguntaView(DetailView):
+    model = Question
+    template_name = 'preguntas/detalle_pregunta.html'
+    context_object_name = 'pregunta'
+
+class ListaPartidasView(ListView):
+    model = Partida
+    template_name = 'preguntas/lista_partidas.html'
+    context_object_name = 'partidas'
